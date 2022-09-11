1b146483e373b0724f857d68be5f37f4149ad91f
LisetteAntigua <antigua223@gmail.com>
2022-09-10 23:41:43 -0400

    tdcc

--- Lab - Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
+++ Lab - Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
@@ -10,7 +10,3 @@
 
 //:  Are the values of `multipliedAsIntegers` and `multipliedAsDoubles` different? Print a statement to the console explaining why.
 
-
-/*:
-[Previous](@previous)  |  page 7 of 8  |  [Next: App Exercise - Converting Types](@next)
- */
